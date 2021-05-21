dc85394fe580bb6bcb7c2807cf902fbc8677158d
Vladimir <Vizion84@yandex.ru>
2021-05-21 23:18:34 +0300

    Refactor code

--- src/components/MessageField/MessageField.js
+++ src/components/MessageField/MessageField.js
@@ -45,5 +45,4 @@ export class MessageField extends React.Component {
       </div>
     )
   }
-
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { MessageField } from './components/MessageField/MessageField'
 
-
-
 import './index.css'
 
 
 class App extends React.Component {
   
-  constructor(props) {
-    super(props);
-    
-
-    this.state = {
-      counter: 0,
-      messages: []
-    };
-    
-  }
-  
-
   render() {
-    console.log("render");
     return (
       <div className='app__container'> 
         <MessageField/>
@@ -31,6 +16,5 @@ class App extends React.Component {
   }
 }
 
-ReactDom.render(
-    <App />, document.getElementById("root"));
+ReactDom.render(<App/>, document.getElementById("root"));
 
