a976d99ef01c0e1397eb3f2b897beb493de0c008
Vladimir <Vizion84@yandex.ru>
2021-05-21 23:26:28 +0300

    Refactor_2

--- src/components/MessageField/Message/Message.js
+++ src/components/MessageField/Message/Message.js
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import  './message.css'
+import './message.css'
 export class Message extends React.Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
-    
+
   }
   constructor(props) {
     super(props);
   }
 
   render() {
-      return <p className='message__text'>{this.props.text}</p>
+    return <p className='message__text'>{this.props.text}</p>
   }
 
-  
+
 }
